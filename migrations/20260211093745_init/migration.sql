8a68b97310f126c8a21613689c73094e686f3c50
sociozaza1412-ai <sociozaza1412@gmail.com>
2026-02-19 17:19:13 +0700

    New Update

---
+++ migrations/20260211093745_init/migration.sql
@@ -0,0 +1,7 @@
+-- CreateTable
+CREATE TABLE "admin" (
+    "id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+    "username" TEXT NOT NULL,
+    "phone" TEXT NOT NULL,
+    "password" TEXT NOT NULL
+);
